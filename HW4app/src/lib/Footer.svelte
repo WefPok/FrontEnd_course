8d321b96b4a2d16ca9b9e63c8c5c3f339507b39d
WefPok <nik4zerg@gmail.com>
2023-03-15 20:31:37 +0300

    HomeWork4

---
+++ HW4app/src/lib/Footer.svelte
@@ -0,0 +1,50 @@
+<footer>
+    <a href="https://github.com/WefPok"><img
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub"
+            id="gh-link"></a>
+    <a href="https://web.telegram.org/k/"><img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/512px-Telegram_logo.svg.png"
+            alt="Telegram" id="tg-link"></a>
+    <a href="mailto:nikita@mail.com"><img src="https://cdn.onlinewebfonts.com/svg/download_262951.png" alt="Email"
+                                          id="email"></a>
+</footer>
+<style>
+    footer {
+        background-color: #333;
+        color: #fff;
+        padding: 20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+
+    footer a {
+        margin: 0 10px;
+    }
+
+
+    footer img {
+        width: 30px;
+        height: 30px;
+        filter: invert(1);
+    }
+
+    .card img {
+        width: auto;
+        height: 100px;
+        border-radius: 5px;
+        margin-bottom: 10px;
+    }
+
+    .card h3 {
+        font-size: 24px;
+        margin-bottom: 10px;
+    }
+
+    .card p {
+        font-size: 18px;
+        line-height: 1.5;
+        color: #666;
+    }
+</style>
---
+++ HW4app/src/lib/Header.svelte
@@ -0,0 +1,40 @@
+<header>
+    <div id="header">
+        <h1>Nikita Noskov</h1>
+    </div>
+</header>
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+    }
+
+    h1 {
+        text-align: left;
+        color: mintcream;
+        margin: 0;
+        padding: 8px 0;
+    }
+
+    h2 {
+        margin-top: 30px;
+    }
+
+
+    p {
+        margin-top: 10px;
+    }
+
+    #header {
+        width: 80%;
+        margin:  auto;
+        vertical-align: middle;
+    }
+    header{
+
+        height: 50px;
+        background-color: #333;
+    }
+
+</style>
