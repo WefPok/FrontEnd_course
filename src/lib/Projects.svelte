60688ad6317e88f2f747221d255d33f9d6f1b1b2
WefPok <nik4zerg@gmail.com>
2023-03-23 13:25:29 +0300

    Images locally

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { base } from '$app/paths'
-
+    import img1 from "$lib/public/assests/1.jpg";
+    import img2 from "$lib/public/assests/2.jpg";
+    import img3 from "$lib/public/assests/3.jpg";
 </script>
 
 <h2>My Projects</h2>
@@ -8,18 +9,18 @@
 
 <div class="card-container">
     <div class="card">
-        <img src={'https://hips.hearstapps.com/hmg-prod/images/domestic-cat-lies-in-a-basket-with-a-knitted-royalty-free-image-1592337336.jpg?crop=0.668xw:1.00xh;0.247xw,0&resize=1200:*'} alt="Card 1">
+        <img src="{img1}" alt="Card 1">
         <p>FlexCard 1</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
     <div class="card">
-        <img src={'https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403'}
+        <img src={img2}
              alt="Card 2">
         <p>FlexCard 2</p>
         <p>Nullam non est cursus, finibus velit sed, vestibulum ante.</p>
     </div>
     <div class="card">
-        <img src={'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/767px-Cat_November_2010-1a.jpg'}
+        <img src={img3}
              alt="Card 3">
         <p>FlexCard 3</p>
         <p>Pellentesque accumsan est eget metus volutpat, ac blandit quam consequat.</p>
