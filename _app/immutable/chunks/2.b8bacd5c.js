6e623cea0a9227d641441b190667a3d85a1120eb
WefPok <46328702+WefPok@users.noreply.github.com>
2023-03-23 10:15:03 +0000

    Deploying to gh-pages from @ WefPok/FrontEnd_course@a9129a216131ed0e21e2875d435ecf552d863e79 🚀

---
+++ _app/immutable/chunks/2.b8bacd5c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.e33ff6ab.js";export{t as component};
---
+++ _app/immutable/entry/_page.svelte.e33ff6ab.js
@@ -0,0 +1,55 @@
+import{S as x,i as $,s as tt,k as d,q as I,a as D,l as f,m as w,r as q,h as u,c as C,n as a,E as Y,b as K,C as e,K as ht,u as ot,D as J,L as vt,w as pt,y as ct,z as mt,A as ut,g as dt,d as ft,B as gt,o as _t}from"../chunks/index.0f2d7c52.js";import"../chunks/paths.1cc55709.js";/* empty css                                                    */function yt(s){let t,i,g,r,o,m,P,h,v=new Date(s[2],s[3]-1,s[4]).toLocaleDateString()+"",l,c,n,_,y,b,E,N,S,M;return{c(){t=d("div"),i=d("h2"),g=I("Free comics"),r=D(),o=d("p"),m=I(s[1]),P=D(),h=d("p"),l=I(v),c=D(),n=d("img"),b=D(),E=d("button"),N=I("Learn More"),this.h()},l(j){t=f(j,"DIV",{class:!0});var p=w(t);i=f(p,"H2",{id:!0,class:!0});var F=w(i);g=q(F,"Free comics"),F.forEach(u),r=C(p),o=f(p,"P",{id:!0,class:!0});var G=w(o);m=q(G,s[1]),G.forEach(u),P=C(p),h=f(p,"P",{id:!0,class:!0});var T=w(h);l=q(T,v),T.forEach(u),c=C(p),n=f(p,"IMG",{src:!0,alt:!0,id:!0}),b=C(p),E=f(p,"BUTTON",{class:!0,id:!0});var k=w(E);N=q(k,"Learn More"),k.forEach(u),p.forEach(u),this.h()},h(){a(i,"id","title"),a(i,"class","svelte-tq7mif"),a(o,"id","text1"),a(o,"class","svelte-tq7mif"),a(h,"id","text2"),a(h,"class","svelte-tq7mif"),Y(n.src,_=s[0].src)||a(n,"src",_),a(n,"alt",y=s[0].alt),a(n,"id","image"),a(E,"class","cta-button"),a(E,"id","button-comic"),a(t,"class","content-block")},m(j,p){K(j,t,p),e(t,i),e(i,g),e(t,r),e(t,o),e(o,m),e(t,P),e(t,h),e(h,l),e(t,c),e(t,n),s[6](n),e(t,b),e(t,E),e(E,N),S||(M=ht(E,"click",function(){vt(s[5])&&s[5].apply(this,arguments)}),S=!0)},p(j,[p]){s=j,p&2&&ot(m,s[1]),p&28&&v!==(v=new Date(s[2],s[3]-1,s[4]).toLocaleDateString()+"")&&ot(l,v),p&1&&!Y(n.src,_=s[0].src)&&a(n,"src",_),p&1&&y!==(y=s[0].alt)&&a(n,"alt",y)},i:J,o:J,d(j){j&&u(t),s[6](null),S=!1,M()}}}function wt(s,t,i){let{title:g}=t,{year:r}=t,{month:o}=t,{day:m}=t,{imgParams:P}=t,{comicDownload:h}=t;function v(l){pt[l?"unshift":"push"](()=>{P.comicImg=l,i(0,P)})}return s.$$set=l=>{"title"in l&&i(1,g=l.title),"year"in l&&i(2,r=l.year),"month"in l&&i(3,o=l.month),"day"in l&&i(4,m=l.day),"imgParams"in l&&i(0,P=l.imgParams),"comicDownload"in l&&i(5,h=l.comicDownload)},[P,g,r,o,m,h,v]}class bt extends x{constructor(t){super(),$(this,t,wt,yt,tt,{title:1,year:2,month:3,day:4,imgParams:0,comicDownload:5})}}function Pt(s){let t,i,g,r,o,m,P,h,v,l,c,n,_,y,b,E,N,S,M,j,p,F,G,T,k,V,et,Q,A,W,X,B,Z;return{c(){t=d("h2"),i=I("My Projects"),g=D(),r=d("div"),o=d("div"),m=d("img"),h=D(),v=d("p"),l=I("FlexCard 1"),c=D(),n=d("p"),_=I("Lorem ipsum dolor sit amet, consectetur adipiscing elit."),y=D(),b=d("div"),E=d("img"),S=D(),M=d("p"),j=I("FlexCard 2"),p=D(),F=d("p"),G=I("Nullam non est cursus, finibus velit sed, vestibulum ante."),T=D(),k=d("div"),V=d("img"),Q=D(),A=d("p"),W=I("FlexCard 3"),X=D(),B=d("p"),Z=I("Pellentesque accumsan est eget metus volutpat, ac blandit quam consequat."),this.h()},l(L){t=f(L,"H2",{class:!0});var H=w(t);i=q(H,"My Projects"),H.forEach(u),g=C(L),r=f(L,"DIV",{class:!0});var U=w(r);o=f(U,"DIV",{class:!0});var z=w(o);m=f(z,"IMG",{src:!0,alt:!0,class:!0}),h=C(z),v=f(z,"P",{class:!0});var at=w(v);l=q(at,"FlexCard 1"),at.forEach(u),c=C(z),n=f(z,"P",{class:!0});var st=w(n);_=q(st,"Lorem ipsum dolor sit amet, consectetur adipiscing elit."),st.forEach(u),z.forEach(u),y=C(U),b=f(U,"DIV",{class:!0});var O=w(b);E=f(O,"IMG",{src:!0,alt:!0,class:!0}),S=C(O),M=f(O,"P",{class:!0});var nt=w(M);j=q(nt,"FlexCard 2"),nt.forEach(u),p=C(O),F=f(O,"P",{class:!0});var lt=w(F);G=q(lt,"Nullam non est cursus, finibus velit sed, vestibulum ante."),lt.forEach(u),O.forEach(u),T=C(U),k=f(U,"DIV",{class:!0});var R=w(k);V=f(R,"IMG",{src:!0,alt:!0,class:!0}),Q=C(R),A=f(R,"P",{class:!0});var it=w(A);W=q(it,"FlexCard 3"),it.forEach(u),X=C(R),B=f(R,"P",{class:!0});var rt=w(B);Z=q(rt,"Pellentesque accumsan est eget metus volutpat, ac blandit quam consequat."),rt.forEach(u),R.forEach(u),U.forEach(u),this.h()},h(){a(t,"class","svelte-1og1vwy"),Y(m.src,P="https://hips.hearstapps.com/hmg-prod/images/domestic-cat-lies-in-a-basket-with-a-knitted-royalty-free-image-1592337336.jpg?crop=0.668xw:1.00xh;0.247xw,0&resize=1200:*")||a(m,"src",P),a(m,"alt","Card 1"),a(m,"class","svelte-1og1vwy"),a(v,"class","svelte-1og1vwy"),a(n,"class","svelte-1og1vwy"),a(o,"class","card svelte-1og1vwy"),Y(E.src,N="https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403")||a(E,"src",N),a(E,"alt","Card 2"),a(E,"class","svelte-1og1vwy"),a(M,"class","svelte-1og1vwy"),a(F,"class","svelte-1og1vwy"),a(b,"class","card svelte-1og1vwy"),Y(V.src,et="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/767px-Cat_November_2010-1a.jpg")||a(V,"src",et),a(V,"alt","Card 3"),a(V,"class","svelte-1og1vwy"),a(A,"class","svelte-1og1vwy"),a(B,"class","svelte-1og1vwy"),a(k,"class","card svelte-1og1vwy"),a(r,"class","card-container svelte-1og1vwy")},m(L,H){K(L,t,H),e(t,i),K(L,g,H),K(L,r,H),e(r,o),e(o,m),e(o,h),e(o,v),e(v,l),e(o,c),e(o,n),e(n,_),e(r,y),e(r,b),e(b,E),e(b,S),e(b,M),e(M,j),e(b,p),e(b,F),e(F,G),e(r,T),e(r,k),e(k,V),e(k,Q),e(k,A),e(A,W),e(k,X),e(k,B),e(B,Z)},p:J,i:J,o:J,d(L){L&&u(t),L&&u(g),L&&u(r)}}}class Et extends x{constructor(t){super(),$(this,t,null,Pt,tt,{})}}function Dt(s){let t,i,g,r,o,m,P,h,v,l;return r=new bt({props:{title:s[0],year:s[1],month:s[2],day:s[3],comicDownload:s[6],imgParams:{src:s[5],alt:s[4],comicImg:s[5]}}}),m=new Et({}),{c(){t=d("html"),i=d("body"),g=d("div"),ct(r.$$.fragment),o=D(),ct(m.$$.fragment),P=D(),h=d("style"),v=I(`body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+    }
+
+    h1 {
+        text-align: left;
+        color: mintcream;
+        margin: 0;
+        padding: 8px 0;
+    }
+
+    h2 {
+        margin-top: 30px;
+    }
+
+
+    p {
+        margin-top: 10px;
+    }
+
+
+    #main_block{
+        width: 80%;
+        margin:  auto;
+        vertical-align: middle;
+    }`),this.h()},l(c){t=f(c,"HTML",{});var n=w(t);i=f(n,"BODY",{});var _=w(i);g=f(_,"DIV",{id:!0});var y=w(g);mt(r.$$.fragment,y),o=C(y),mt(m.$$.fragment,y),y.forEach(u),_.forEach(u),P=C(n),h=f(n,"STYLE",{});var b=w(h);v=q(b,`body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+    }
+
+    h1 {
+        text-align: left;
+        color: mintcream;
+        margin: 0;
+        padding: 8px 0;
+    }
+
+    h2 {
+        margin-top: 30px;
+    }
+
+
+    p {
+        margin-top: 10px;
+    }
+
+
+    #main_block{
+        width: 80%;
+        margin:  auto;
+        vertical-align: middle;
+    }`),b.forEach(u),n.forEach(u),this.h()},h(){a(g,"id","main_block")},m(c,n){K(c,t,n),e(t,i),e(i,g),ut(r,g,null),e(g,o),ut(m,g,null),e(t,P),e(t,h),e(h,v),l=!0},p(c,[n]){const _={};n&1&&(_.title=c[0]),n&2&&(_.year=c[1]),n&4&&(_.month=c[2]),n&8&&(_.day=c[3]),n&48&&(_.imgParams={src:c[5],alt:c[4],comicImg:c[5]}),r.$set(_)},i(c){l||(dt(r.$$.fragment,c),dt(m.$$.fragment,c),l=!0)},o(c){ft(r.$$.fragment,c),ft(m.$$.fragment,c),l=!1},d(c){c&&u(t),gt(r),gt(m)}}}function Ct(s,t,i){let g="",r="",o="",m="",P="",h="";async function v(){try{const l=new URLSearchParams({email:"n.noskov@innopolis.university"}),n=await(await fetch(`https://fwd.innopolis.app/api/hw2?${l}`)).json();console.log("res:",n);const _=new URLSearchParams(n.toString()),y=await fetch(`https://getxkcd.vercel.app/api/comic?${_}`).then(b=>b.json());i(0,g=y.title),i(1,r=y.year),i(2,o=y.month),i(3,m=y.day),i(4,P=y.alt),i(5,h=y.img)}catch(l){console.error(l)}}return _t(async()=>{await v()}),[g,r,o,m,P,h,v]}class jt extends x{constructor(t){super(),$(this,t,Ct,Dt,tt,{})}}export{jt as default};
